054eddb2f8c543b4b7b8e180ac64248b34a02b65
namratak277 <namratak277@gmail.com>
2025-12-09 17:39:24 -0500

    vercel2

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a58703b9bd080901"; }
+/******/ 		__webpack_require__.h = function() { return "561ad6f4cc5b877c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
